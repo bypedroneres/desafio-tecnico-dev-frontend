9b6a566815d7e0a0c372db41bbea34ea212fc007
Pedro Neres <bypedroneres@gmail.com>
2024-02-29 12:30:29 -0300

    trying to achieve lighthouse 100% again

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
           <div className="flex items-center">
-            <img src="/logo.png" alt="Logo" className="h-10" />
+            <img src="/logo.png" alt="Logo" className="h-10 w-12" />
           </div>
           <div className="hidden md:flex items-center space-x-6">
             <a href="#" className="text-gray-800 hover:text-gray-500">Home</a>
