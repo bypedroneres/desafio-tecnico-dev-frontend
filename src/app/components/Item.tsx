b43393b32665605d45c71accefbd486cd9880da1
Pedro Neres <bypedroneres@gmail.com>
2024-02-29 11:17:02 -0300

    mapping the star array and othe small changes

--- src/app/components/Item.tsx
+++ src/app/components/Item.tsx
@@ -11,13 +11,12 @@ interface ItemProps {
 }
 
 const Item: React.FC<ItemProps> = ({ item }) => {
-  const renderRatingStars = () => {
-    let stars = '';
-    for (let i = 0; i < item.rating; i++) {
-      stars += '★';
-    }
+  const renderRatingStars = React.useMemo(() => {
+    const stars = Array.from({ length: item.rating }, (_, index) => (
+      <span key={index} className="mr-2">&#9733;</span>
+    ));
     return stars;
-  };
+  }, [item.rating]);
 
   return (
     <tr className='border border-gray-100 hover:bg-gray-100 cursor-pointer'>
@@ -34,7 +33,7 @@ const Item: React.FC<ItemProps> = ({ item }) => {
           {item.status}
         </span>
       </td>
-      <td className="py-2 pl-6 hidden lg:table-cell">{renderRatingStars()}</td>
+      <td className="py-2 pl-6 hidden lg:table-cell">{renderRatingStars}</td>
       <td className="py-2 pl-12 hidden lg:table-cell">
         <div className="actions">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 24" width="10" height="24">
@@ -48,22 +47,18 @@ const Item: React.FC<ItemProps> = ({ item }) => {
       {/* Mobile style */}
       <td className="py-2 pl-4 lg:hidden relative">
         <div className="car-info flex items-center">
-          {/* Car image container */}
           <div className="relative">
-            {/* Sticker-like availability indicator */}
             <div className="absolute top-0 left-0">
               <span className={`text-2xl flex items-center justify-center ${item.status === 'Available' ? 'bg-green-400 text-white' : 'bg-red-400 text-white'} rounded-full h-8 w-8`}>
                 {item.status === 'Available' ? '\u2713' : 'x'}
               </span>
             </div>
-            {/* Car image */}
             <div className="car-image h-24 w-24 rounded" style={{ backgroundImage: `url(${item.car})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
           </div>
-          {/* Car details */}
           <div className="flex-grow px-4">
             <p className="text-base font-semibold">{item.carname}</p>
             <p className="text-sm text-gray-500">Next reservation: {item.nextReservation}</p>
-            <p className="text-base text-black">{renderRatingStars()}</p>
+            <p className="text-base text-black">{renderRatingStars}</p>
           </div>
           {/* Action */}
           <div className="actions flex w-1/6">
